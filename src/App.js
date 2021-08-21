9c425f89181fcfcf32e68f55961049518896ab8b
Durrez Ahmed <durrezahmed3@gmail.com>
2021-08-21 12:37:09 +0530

    add bootstrap and navbar to text-utils-react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar title='TextUtils' aboutText='About' />
+    </>
+  );
+}
+
+export default App;
